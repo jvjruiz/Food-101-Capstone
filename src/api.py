a44252ae16730ace17d6c915dcc4cecc3c6e9b01
Jo Vincent Ruiz <jovincent.ruiz@gmail.com>
2020-01-21 18:40:52 -0800

    continued working on api

--- src/api.py
+++ src/api.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 import jsonpickle
 import numpy as np
-import cv2
 import requests
 from utils.dataset import get_labels
 from PIL import Image
 
 from utils.dataset import resize_image
+
+import pdb
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -16,34 +17,33 @@ SERVER_URL = 'http://localhost:8501/v1/models/'
 # route http posts to this method
 @app.route('/api/predict_food', methods=['POST'])
 def predict_food():
-    app.logger.debug(request)
+    print('hi')
     r = request
+    print(r.files)
+    # # decode image
+    # img = r.files[0]
 
-    # decode image
-    img = r.files[0]
+    # # do some fancy processing here....
+    # resized_image = resize_image(img)
+    # _, resized_img_encode = cv2.imencode('.jpg', resized_image)
 
-    # do some fancy processing here....
-    resized_image = resize_image(img)
-    _, resized_img_encode = cv2.imencode('.jpg', resized_image)
+    # predict_response = requests.post(SERVER_URL, data=resize_image_encode.tostring())
+    # predict_response.raise_for_status()
+    # predict_response = predict_response.json()
 
-    predict_response = requests.post(SERVER_URL, data=resize_image_encode.tostring())
-    predict_response.raise_for_status()
-    predict_response = predict_response.json()
+    # y_prob = np.array(response['predictions'])
+    # label_index = np.argmax(y_prob)
 
-    y_prob = np.array(response['predictions'])
-    label_index = np.argmax(y_prob)
-
-    # get food labels
-    labels = get_labels()
-    label = labels[label_index]
+    # # get food labels
+    # labels = get_labels()
+    # label = labels[label_index]
 
     # build a response dict to send back to client
-    response = {'message': 'Predicted guess for food is {}'.format(label)
-                }
-    # encode response using jsonpickle
-    response_pickled = jsonpickle.encode(response)
+    label = 'ramen'
+    response = jsonify(message='this is the label {}'.format(label), status=200)
 
-    return Response(response=response_pickled, status=200, mimetype="application/json")
+    print(response)
+    return response
 
 
 # start flask app
--- src/test_client.py
+++ src/test_client.py
@@ -4,6 +4,8 @@ import json
 from PIL import Image
 import io
 
+import pdb
+
 addr = 'http://localhost:5000'
 test_url = addr + '/api/predict_food'
 
@@ -15,10 +17,18 @@ img = Image.open('../data/images/apple_pie/134.jpg')
 imgByteArr = io.BytesIO()
 img.save(imgByteArr,format='jpeg')
 
+files = {'file': ('134.jpg', imgByteArr, content_type)}
 
 # send http request with image and receive response
-response = requests.post(test_url, 
-files={'file': ('134.jpg', imgByteArr, content_type)})
-# decode response
-print(json.loads(response.text))
+# response = requests.post(test_url, 
+# files=files
+# )
+# print(response)
+# # decode response
+# print(json.loads(response.text))
 
+with requests.Session() as s:
+    response = s.post(test_url,files=files)
+    response = json.loads(response.content)
+    pdb.set_trace()
+    print(response)
